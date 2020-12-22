b1480f9333e5ed20d377379d8f2954906a5ee9a7
mamuritto <66204169+mamuritto@users.noreply.github.com>
2020-12-23 01:15:03 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+(function () {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active')
+    })
+}());
 
-
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.navbar');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.toggle('burger__active');
+    });
+    
+}());
 
